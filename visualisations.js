f3c4f6ef45a76b091341f1850433923f8d7cf48c
Ding Cong <LimDingCong101@gmail.com>
2025-10-19 19:18:22 +0800

    Add files via upload

---
+++ visualisations.js
@@ -0,0 +1,30 @@
+var vg_1 = "map.vg.json";
+var vg_2 = "vis2.vg.json";
+var vg_3 = "vis3.vg.json";
+var vg_4 = "vis4.vg.json";
+var vg_5 = "vis5.vg.json";
+
+vegaEmbed("#map_vis", vg_1)
+.then(function(result) {
+})
+.catch(console.error);
+
+vegaEmbed("#bar_vis", vg_2)
+.then(function(result) {
+})
+.catch(console.error);
+
+vegaEmbed("#line_vis", vg_3)
+.then(function(result) {
+})
+.catch(console.error);
+
+vegaEmbed("#pie_vis", vg_4)
+.then(function(result) {
+})
+.catch(console.error);
+
+vegaEmbed("#splot_vis", vg_5)
+.then(function(result) {
+})
+.catch(console.error);
